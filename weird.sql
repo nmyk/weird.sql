d3efe0d914be20b76bd70a793beecf569f8c9b08
nmyk <nick.mykins@gmail.com>
2020-04-24 15:29:00 -0400

    fix one that didn't run. capitalize all Xs

--- weird.sql
+++ weird.sql
@@ -11,13 +11,13 @@ ALTER TABLE X RENAME TO """";
 SELECT EXISTS(SELECT);
 
 -- ????
-SELECT * FROM (SELECT);
+SELECT * FROM (SELECT) X;
 
 -- select predicate not evaluated in EXISTS
 SELECT EXISTS(SELECT 1/0 FROM """");
 
 -- "successful insert" into table with FALSE check
-ALTER TABLE """" ADD CONSTRAINT x CHECK (FALSE);
+ALTER TABLE """" ADD CONSTRAINT X CHECK (FALSE);
 INSERT INTO """" SELECT * FROM """";
 
 -- returns 1
